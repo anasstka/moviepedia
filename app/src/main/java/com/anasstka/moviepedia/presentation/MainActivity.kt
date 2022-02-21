62e87c0b39df7d06eb8a920655890d658596e92d
Anastasia Tsvetkova <nastyushka20020711@gmail.com>
2022-02-21 21:36:06 +0600

    changing the MainActivity

--- app/src/main/java/com/anasstka/moviepedia/presentation/MainActivity.kt
+++ app/src/main/java/com/anasstka/moviepedia/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anasstka.moviepedia
+package com.anasstka.moviepedia.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,31 +8,26 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import coil.annotation.ExperimentalCoilApi
+import com.anasstka.moviepedia.presentation.movies.MoviesScreen
 import com.anasstka.moviepedia.ui.theme.MoviepediaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalCoilApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MoviepediaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    MoviesScreen(navController = navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MoviepediaTheme {
-        Greeting("Android")
     }
 }
